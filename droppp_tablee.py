67698fad73b8032e01f866546cf1f61174ed6be3
Aswanth-s-382 <110896995+Aswanth-s-382@users.noreply.github.com>
2022-09-12 15:19:36 +0530

    Add files via upload

---
+++ droppp_tablee.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+conn=mysql.connector.connect(
+    user='root',password='abc123',host='127.0.0.1',database='mysql'
+)f
+cursor=conn.cursor()
+cursor.execute("SHOW Tables")
+print(cursor.fetchall())
+
+cursor.execute("DROP TABLE EMPLOYEE")
+print("Table dropped...")
+
+print(
+    "List of tables after dropping the EMPLOYEE table:")
+cursor.execute("SHOW tables")
+print(cursor.fetchall())
+conn.close()
